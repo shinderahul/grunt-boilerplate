a14f8b7a0dd803674e3de3a0e96a15de47fea245
Rahul Shinde <rahul.shinde@digitaslbi.com>
2018-04-06 17:19:07 +0530

    Added grunt concat

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: ['src/assets/js/*.js', 'src/assets/js/vendor/*.js'],
+        dest: 'build/assets/js/script.js'
+      },
+      css: {
+        src: ['src/assets/css/*.css', 'src/assets/css/vendor/*.css'],
+        dest: 'build/assets/css/style.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+};
